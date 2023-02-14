89d14e2f73909230b8efce6c152ff4aa05a6de63
Ritsuha <mharis830174@gmail.com>
2023-02-14 19:36:50 +0700

    Assignment1

---
+++ Assignment 1/Assignment/Assignment1/p2.go
@@ -0,0 +1,56 @@
+package Assignment1
+
+import (
+    "unicode"
+    "strings"
+    "regexp"
+)
+/*
+Clean up sentences. [WEIGHT = 1]
+
+Given a string, you need to clean up and remove unnecessary characters.
+The only characters that are allowed are:
+    - letters (a-z or A-Z)
+    - numbers (0-9)
+    - space character (" ")
+
+Replace every other characters with a single space. And then, remove all spaces
+in the beginning and at the end of the string (leading/trailing spaces).
+
+Finally, return the resulting string.
+
+Examples
+------------------------------------
+Input: "I am learning Go???"
+Output: "I am learning Go"
+Explanation: Remove the question marks at the end
+------------------------------------
+Input: "Everything is good"
+Output: "Everything is good"
+Explanation: Nothing changes so return string as is
+------------------------------------
+Input: "    Independence day: August 17th, 1945 "
+Output: "Independence day August 17th 1945"
+Explanation: Replace the : and , and then remove all trailing/leading spaces
+------------------------------------
+*/
+
+func CleanUp(s string) string {
+	// TODO: Your code here
+    cleaned := ""
+    space := regexp.MustCompile(`\s+`)
+    for _, val := range s {
+        if unicode.IsLetter(val) || unicode.IsNumber(val) || unicode.IsSpace(val) {
+            cleaned += string(val)
+        }
+    }
+
+    // Remove leading and trailing spaces
+    trimCleaned := strings.TrimSpace(cleaned)
+
+    // Remove duplicate space
+    result := space.ReplaceAllString(trimCleaned, " ")
+
+    return result
+}
+
